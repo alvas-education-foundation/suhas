bdadead2efa7ec1755f13ff7d7a16e67751a4aaf
4al16cs080 <suhaspshetty98@gmail.com>
2020-06-12 21:28:39 +0530

    certification

---
+++ coding/day14/prog.c
@@ -0,0 +1,23 @@
+#include
+
+using namespacestd;
+
+intmain()
+
+{
+
+intnumber=147;
+
+//Any number.intres;
+
+if(number) res=number%9==0?9:
+
+number%9;
+
+else res=0;
+
+//print the result cout<<res;
+
+Return 0;
+
+}
---
+++ coding/day16/prog.py
@@ -0,0 +1,19 @@
+Python program to find H.C.F of two numbers
+
+defcompute_hcf(x,y):
+
+ifx>y:
+
+smaller=y else:
+
+smaller=x foriinrange(1,smaller+1):
+
+if((x%i==0)and(y%i==0)):
+
+hcf=i return hcf
+
+num1=54
+
+num2=24
+
+print("TheH.C.F.is",compute_hcf(num1,num2))
